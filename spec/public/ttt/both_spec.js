7f01c15853b1029820dd69db423b7f57986b83a0
Steven Parkes <smparkes@smparkes.net>
2009-12-16 10:47:11 -0800

    vendor setup; start work on jsrat spec

---
+++ spec/public/ttt/both_spec.js
@@ -0,0 +1,18 @@
+(function($){
+
+  describe("ttt",function(){
+    describe("both",function(){
+
+      describe("starting a server",function(){
+        $.click_link("Start server");
+        $.click_link("Join a game");
+        $.click_link("Join a game");
+        for(var move = 0, player = 0; move < 9; move++, player = 1-player) {
+          $(".game").eq(player).click_link(move);
+        }
+      });
+
+    });
+  });
+
+})(jQuery);
