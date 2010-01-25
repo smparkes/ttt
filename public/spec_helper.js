73a6130515cf27badddfcdff06fcdb6de93b756b
Steven Parkes <smparkes@smparkes.net>
2010-01-25 12:31:16 -0800

    tweak jslint options/defs

--- public/spec_helper.js
+++ public/spec_helper.js
@@ -1,4 +1,5 @@
 /*jslint evil: true*/
+/*global jazrb_root, window, document*/
 "use strict";
-var jazrb_root = (function(){return this;}()).jazrb_root || "..";
-window.document.write("<script type='text/javascript' src='" + jazrb_root + "/spec/spec_helper.js'></script>");
+window.jazrb_root = window.jazrb_root || "..";
+document.write("<script type='text/javascript' src='" + jazrb_root + "/spec/spec_helper.js'></script>");
