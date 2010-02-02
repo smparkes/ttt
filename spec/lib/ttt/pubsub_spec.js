74e76c7267c7d82d8d9611ee55cf23cee5fbba06
Steven Parkes <smparkes@smparkes.net>
2010-02-02 11:46:51 -0800

    more complete pubsub

--- spec/lib/ttt/pubsub_spec.js
+++ spec/lib/ttt/pubsub_spec.js
@@ -12,6 +12,7 @@ include(jazrb_root + "/spec/lib/ttt/spec_helper.js");
         this.publisher = function(){};
         $.extend(this.publisher.prototype,TTT.Publisher.prototype);
         this.sub = new this.subscriber();
+        this.sub2 = new this.subscriber();
         this.pub = new this.publisher();
       });
 
@@ -30,28 +31,83 @@ include(jazrb_root + "/spec/lib/ttt/spec_helper.js");
         incomplete();
       });
 
-      it("should receive an initial state if asked", function() {
+      it("should receive an initial state", function() {
         var hash = {a: "b"};
         this.sub.method = function method(state) {
           expect(state).toEqual(hash);
           complete();
         };
-        this.pub.update = function update() {
-          this.notify(hash);
-        };
-        this.sub.subscribe({to: this.pub, call: "method", initial: true});
+        this.pub.notify(hash);
+        this.sub.subscribe({to: this.pub, call: "method"});
         incomplete();
       });
 
-      it("should only send state to new object on init request");
-
-      it("should be possible to unsubscribe", function() {
-        pending();
+      it("should not receive an initial state if there is none", function() {
+        var hash = {a: "b"};
+        this.sub.method = function method(state) {
+          expect(false).toBe(true);
+        };
+        setTimeout(function(){
+          complete();
+        },100);
+        this.pub.notify(hash);
+        this.sub.subscribe({to: this.pub, call: "method", state_id: 1});
+        incomplete();
       });
 
-      it("should not receive callbacks after unsub");
+      it("should get first state", function() {
+        var hash = {a: "b"};
+        this.sub.method = function method(state) {
+          expect(state).toEqual(hash);
+          complete();
+        };
+        this.sub.subscribe({to: this.pub, call: "method"});
+        this.pub.notify(hash);
+        incomplete();
+      });
+
+      it("should only send state to new object on init request", function() {
+        var hash = {a: "b"};
+        this.sub2.method = function method(state) {
+          expect(state).toEqual(hash);
+          this.method = function(state) {
+            expect(true).toBe(false);
+          }
+        };
+        this.sub.method = function method(state) {
+          expect(state).toEqual(hash);
+        };
+        setTimeout(function(){
+          complete();
+        },100);
+        this.sub2.subscribe({to: this.pub, call: "method"});
+        this.pub.notify(hash);
+        this.sub.subscribe({to: this.pub, call: "method"});
+        incomplete();
+      });
+
+      it("should be possible to unsubscribe", function() {
+        this.sub.unsubscribe({to: this.pub, call: "method"});
+      });
+
+      it("should not receive callbacks after unsub", function() {
+        var hash = {a: "b"};
+        this.sub.method = function method(state) {
+          expect(state).toBe(hash);
+          this.method = function (state) {
+            expect(false).toBe(true);
+          }
+        };
+        setTimeout(function(){
+          complete();
+        },100);
+        this.sub.subscribe({to: this.pub, call: "method"});
+        this.pub.notify(hash);
+        this.sub.unsubscribe({to: this.pub, call: "method"});
+        this.pub.notify(hash);
+        incomplete();
+      });
 
     });
-
   });
 }(jQuery));
