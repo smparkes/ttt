3dc295409ccd5fc05cdec509d8c8580c3a783b3a
Steven Parkes <smparkes@smparkes.net>
2010-07-20 13:55:44 -0700

    cleanup

--- public/ttt/actor/ttt.js
+++ public/ttt/actor/ttt.js
@@ -1,38 +1,6 @@
-/*jslint evil: true*/
 "use strict";
+/*jslint evil: true*/
 (function(){
-  var global = (function(){return this;}());
-  global.ttt_top = global.ttt_top || "../../../";
-
+  window.ttt_top = window.ttt_top || "../../../";
   document.write("<script src='../ttt.js' type='text/javascript'></script>");
-
-/*
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/class.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/runtime.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/runtime/callable.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/runtime/callable/method.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/continuation.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/actor.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/future.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/actor/behavior.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/actor/interface.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/actor/name.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/actor/type.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/actor/name/type.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/director.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/runtime/reactor.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/runtime/reactor/channel.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/runtime/reactor/channel/xmpp.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/vendor/strophejs/src/core.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/subscriber.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/vendor/dramatis/lib/dramatis/publisher.js' type='text/javascript'></script>");
-
-  document.write("<script src='"+global.ttt_top+"/lib/ttt/game/view/table/actor.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/lib/ttt/game/view/svg/actor.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/lib/ttt/server/view/actor.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/lib/ttt/server/actor.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/lib/ttt/player/actor.js' type='text/javascript'></script>");
-  document.write("<script src='"+global.ttt_top+"/lib/ttt/game/actor.js' type='text/javascript'></script>");
-*/
 }());
