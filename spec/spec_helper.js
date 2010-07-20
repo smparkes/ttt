8309c8db0dc189152de8c4e9a22ba6ddc83150b2
Steven Parkes <smparkes@smparkes.net>
2010-07-20 13:57:07 -0700

    handle paths in browser

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,7 +1,12 @@
 "use strict";
-if (!window.jQuery) {
-  jazz.include("vendor/jquery/dist/jquery.js");
-}
-if (!window.TTT) {
-  jazz.include("lib/ttt.js");
-}
+(function() {
+  var global = (function(){return this;}());
+  if (!window.jQuery) {
+    jazz.include((jazz.app_root ? jazz.app_root+"/" : "") +
+                 "vendor/jquery/dist/jquery.js");
+  }
+  if (!window.TTT) {
+    jazz.include((jazz.app_root ? jazz.app_root+"/" : "") +
+                 "lib/ttt.js");
+  }
+}());  
